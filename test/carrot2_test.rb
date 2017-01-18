d83ae7241c8e32f8370f8199d6b4f76c2a0d7b89
Andrew Kane <acekane1@gmail.com>
2017-01-17 20:09:11 -0800

    Added working test case

--- test/carrot2_test.rb
+++ test/carrot2_test.rb
@@ -2,6 +2,14 @@ require_relative "test_helper"
 
 class Carrot2Test < Minitest::Test
   def test_basic
-    assert false
+    documents = [
+      "Sign up for an exclusive coupon.",
+      "Exclusive members get a free coupon.",
+      "Coupons are going fast.",
+      "This is completely unrelated to the other documents."
+    ]
+
+    carrot2 = Carrot2.new
+    assert_equal ["Coupon", "Exclusive", "Other Topics"], carrot2.cluster(documents)["clusters"].map { |c| c["phrases"].first }
   end
 end
